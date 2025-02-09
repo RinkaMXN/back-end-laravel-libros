5fb9c83ef490ccf03212dff84221f15b2c33da12
MARCO ANTONIO CADENAS LOPEZ <rinkayolo@gmail.com>
2025-02-09 14:50:25 -0600

    actualizacion de libros

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Book;
 use App\Models\Autor;
@@ -23,7 +24,7 @@ class BooksController extends Controller
         $data = array(
             'status' => 'error',
             'code'   => 200,
-            'message' => 'No se pudo conseguir los libros desde la consulta'
+            'message' => 'Hubo un error en la BD, no se pudieron conseguir los libros, intentelo más tarde'
         ); 
       }
       return response()->json($data, $data['code']);
@@ -42,7 +43,7 @@ class BooksController extends Controller
         $data = array(
             'status' => 'error',
             'code'   => 200,
-            'message' => 'No se pudieron conseguir a los autores desde la consulta'
+            'message' => 'Hubo un error en la BD, no se pudieron conseguir a los autores, intentelo más tarde'
         ); 
       }
       return response()->json($data, $data['code']);
@@ -61,7 +62,7 @@ class BooksController extends Controller
         $data = array(
             'code'   => 200,
             'status' => 'error',
-            'message' => 'No se pudieron conseguir a los géneros desde la consulta'
+            'message' => 'Hubo un error en la BD, no se pudieron conseguir los generos, intentelo más tarde'
         ); 
       }
       return response()->json($data, $data['code']);
@@ -75,7 +76,21 @@ class BooksController extends Controller
         $book->id_genero = $request->id_genero;
         $book->fecha_publicacion_libro = $request->fecha_publicacion_libro;
         $book->descripcion_libro = $request->descripcion_libro;
-        $book->imagen_libro = $request->imagen_libro;
+
+        // Obtiene el último ID
+        $lastId = Book::max('id_libro');
+        // Si no hay registros, comienza desde 1 
+        $nextId = $lastId ? $lastId + 1 : 1; 
+        // Obtener el ID recién creado
+        $bookId = $nextId; 
+
+        $file = $request->file('imagen_libro');
+        $img = $file->getClientOriginalName();
+        $img2 = $bookId . $img;
+        \Storage::disk('local')->put($img2, \File::get($file));
+        
+        //$book->imagen_libro = $img2;
+        $book->imagen_libro = asset('archivos/' . $img2);
         $book->save();
 
         // Respuesta de success
@@ -83,14 +98,98 @@ class BooksController extends Controller
           'status' => 'success',
           'code'   => 200,
           'message' => 'El libro se guardó con éxito'
-        ); 
+        );
 
       }catch(\Illuminate\Database\QueryException $e) {
         // Respuesta de error
         $data = array(
             'status' => 'error',
             'code'   => 200,
-            'message' => 'No se pudo guardar el libro'
+            'message' => 'Hubo un error en la BD, no se pudo crear el libro, intentelo más tarde'
+        ); 
+      }
+      return response()->json($data, $data['code']);
+    }
+
+    public function getBookById($id){
+      try{
+
+        $book = DB::table('libros as l')
+        ->select(
+            'l.id_libro',
+            'l.titulo_libro',
+            'l.id_autor',
+            'l.descripcion_libro',
+            'l.id_genero',
+            'l.fecha_publicacion_libro',
+            'l.imagen_libro',
+            )
+        ->where('id_libro', $id)
+        ->first();
+
+        // Preparar la respuesta, regresamos lo de los inputs y la tabla.
+        $data = array(
+          'status' => 'success',
+          'code' => 200,
+          'books' => $book,
+        );
+
+      }catch(\Illuminate\Database\QueryException $e) {
+        // Respuesta de error
+        $data = array(
+            'status' => 'error',
+            'code'   => 200,
+            'message' => 'Hubo un error en la BD, no se pudo conseguir los datos del libro, intentelo más tarde'
+        ); 
+      }
+      return response()->json($data, $data['code']);
+    }
+
+    public function updatebook(Request $request, $id){
+      try{
+        // Buscar el libro por ID
+        $book = Book::find($id);
+    
+        if (!$book) {
+            return response()->json([
+                'status' => 'error',
+                'code' => 404,
+                'message' => 'Libro no encontrado'
+            ], 404);
+        }
+
+        $book->titulo_libro = $request->titulo_libro;
+        $book->id_genero = $request->id_genero;
+        $book->id_autor = $request->id_autor;
+        $book->fecha_publicacion_libro = $request->fecha_publicacion_libro;
+        $book->descripcion_libro = $request->descripcion_libro;
+        // Subir archivo de evidencia, si existe
+        if ($request->hasFile('imagen_libro')) {
+          // Opcional: eliminar evidencia asociada, si existe
+          if ($book->imagen_libro && file_exists(public_path('archivos/' . $book->imagen_libro))) {
+              unlink(public_path('archivos/' . $book->imagen_libro));
+          }
+          $file = $request->file('imagen_libro');
+          $img = $file->getClientOriginalName();
+          $img2 = $book->id_libro . $img;
+          \Storage::disk('local')->put($img2, \File::get($file));
+          $book->imagen_libro = asset('archivos/' . $img2);
+        }
+        $book->save();
+
+        // Preparar la respuesta, regresamos lo de los inputs y la tabla.
+        $data = array(
+          'status' => 'success',
+          'code' => 200,
+          'message' => 'El libro se actualizó con éxito'
+        );
+
+      }catch(\Illuminate\Database\QueryException $e) {
+        // Respuesta de error
+        $data = array(
+            'status' => 'error',
+            'code'   => 200,
+            'message' => 'Hubo un error en la BD, no se pudo actualizar el libro, intentelo más tarde'
         ); 
       }
       return response()->json($data, $data['code']);
--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,5 @@ Route::get("/index", [App\Http\Controllers\BooksController::class, "index"])->na
 Route::get("/getAutoresAll", [App\Http\Controllers\BooksController::class, "getAutoresAll"])->name("getAutoresAll");
 Route::get("/getGenerosAll", [App\Http\Controllers\BooksController::class, "getGenerosAll"])->name("getGenerosAll");
 Route::post("/createbook", [App\Http\Controllers\BooksController::class, "createbook"])->name("createbook");
+Route::get('/getBookById/{id}', [App\Http\Controllers\BooksController::class, 'getBookById']);
+Route::post("/updatebook/{id}", [App\Http\Controllers\BooksController::class, "updatebook"])->name("updatebook");
